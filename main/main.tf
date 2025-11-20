c5e93d3fcdee3a6032d7dd35d9874da01fad39cd
Dhishan Amaranath <iamdhishan@gmail.com>
2025-11-20 11:28:43 -0800

    adding demo domain

--- main/main.tf
+++ main/main.tf
@@ -18,7 +18,7 @@ locals {
   # Global FQDN (for Route53 geolocation/latency routing)
   # Example: dev.winda.ai (routes to nearest region)
   global_fqdn = "${var.environment}.winda.ai"
-  subdomains  = ["corrosion-engineer", "corrosion-buddy", "corrosion-prediction"]
+  subdomains  = ["demo", "corrosion-engineer", "corrosion-buddy", "corrosion-prediction"]
 }
 ///////////////////////////////////////////////
 // Data Sources
